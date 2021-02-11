fc347946146235a3ced397a3e81c40c35ca9f634
Jarred Parr <jparr721@gmail.com>
2021-02-11 09:45:04 -0700

    use build in functions

--- lattice/lattice/spring.cpp
+++ lattice/lattice/spring.cpp
@@ -7,28 +7,14 @@
 
 #include "spring.hpp"
 
-/**
-    @brief Computes the distance between the two internal mass vectors.
- */
-float Spring::ComputeVectorDistance() {
-    const float x_distance =
-        std::pow(left_mass->Position().x - right_mass->Position().x, 2);
-    const float y_distance =
-        std::pow(left_mass->Position().y - right_mass->Position().y, 2);
-    const float z_distance =
-        std::pow(left_mass->Position().z - right_mass->Position().z, 2);
-
-    return std::sqrt(x_distance + y_distance + z_distance);
-}
-
 void Spring::ApplySpringForces() {
-    const float current_spring_length = ComputeVectorDistance();
+    const float current_spring_length =
+        glm::distance(left_mass->Position(), right_mass->Position());
 
     // Calculate the vector norm so we can express hooke's law as a directional
     // vector
     const glm::vec4 mass_norm =
-        (left_mass->Position() - right_mass->Position()) /
-        current_spring_length;
+        glm::normalize(left_mass->Position() - right_mass->Position());
 
     const glm::vec3 force =
         kStiffness * (current_spring_length - kLength) * mass_norm;
