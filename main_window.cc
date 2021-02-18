8025190f87cb586a63c37b54631c950c8262a246
Jarred Parr <jparr721@gmail.com>
2021-02-18 15:13:24 -0700

    remove unused code

--- main_window.cc
+++ main_window.cc
@@ -6,14 +6,6 @@
 #include <QMessageBox>
 
 MainWindow::MainWindow() {
-    // QMenuBar* menu_bar = new QMenuBar;
-    // QMenu* menu_window = menu_bar->addMenu(tr("&Window"));
-    // QAction* add_new_sim = new QAction(menu_window);
-
-    // add_new_sim->setText(tr("Add New Simulator"));
-    // menu_window->addAction(add_new_sim);
-    // connect(add_new_sim, &QAction::triggered, this, &MainWindow::OnAddNew);
-    // setMenuBar(menu_bar);
     OnAddNew();
 }
 
