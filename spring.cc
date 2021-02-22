9c040e8a1f79d46ee09d6c62e828a79102be9681
Jarred Parr <jparr721@gmail.com>
2021-02-22 10:32:55 -0700

    fix spring to remove old code

--- spring.cc
+++ spring.cc
@@ -15,8 +15,6 @@ void Spring::Initialize() {
     is_init = true;
 }
 
-void Spring::Update(float dt) { ApplySpringForces(); }
-
 void Spring::ComputeVertexPoints() {
     const auto lpos = left_mass->Position();
     const auto rpos = right_mass->Position();
@@ -31,21 +29,6 @@ void Spring::ComputeVertexPoints() {
     vertices = std::vector<Eigen::Vector3f>{{v1, v2, v3}};
 }
 
-void Spring::ApplySpringForces() {
-    const float current_spring_length = utility::EuclideanDistance(
-        left_mass->Position(), right_mass->Position());
-
-    const Eigen::Vector4f mass_norm =
-        (left_mass->Position() - right_mass->Position()).normalized();
-
-    force =
-        stiffness * (current_spring_length - rest_length) * mass_norm.array();
-    Eigen::Vector4f mass_acceleration_delta = force / right_mass->Weight();
-
-    right_mass->ChangeAcceleration(mass_acceleration_delta);
-    left_mass->ChangeAcceleration(mass_acceleration_delta);
-}
-
 Eigen::Vector4f Spring::CalculateCurrentForce(std::shared_ptr<Mass> ref) {
     const float current_spring_length = utility::EuclideanDistance(
         left_mass->Position(), right_mass->Position());
@@ -55,10 +38,11 @@ Eigen::Vector4f Spring::CalculateCurrentForce(std::shared_ptr<Mass> ref) {
     const Eigen::Vector4f force_direction = (
         right_mass->Position() - left_mass->Position()).normalized();
 
-    // Contraction/Expansion rate for damping force
+    // Contraction/Expansion rate for damping force with respect to the direction its applied
     const float left_spring_response = left_mass->Velocity().dot(force_direction);
     const float right_spring_response = right_mass->Velocity().dot(force_direction);
 
+    // Multiply our damping by the sum of the spring forces in either direction.
     const float damping = damping_constant * (left_spring_response + right_spring_response);
 
     if (ref == left_mass) {
